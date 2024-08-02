b658f0787b27d703c0a17ecbe281c7c94b52357f
minamaher58 <minamahertalaat58@gmail.com>
2024-08-02 09:12:41 +0300

    adding orders to a dictionary and update it when the customer adds or changes his order

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import db_helper
+import generic_helper
 
 app = FastAPI()
+
+inprogress_orders = {}
 
 @app.post("/")
 async def handle_request(request: Request):
@@ -15,11 +18,27 @@ async def handle_request(request: Request):
     intent = payload['queryResult']['intent']['displayName']
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
+    session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
 
-    if intent == "track.order - context: ongoing-tracking":
-        return track_order(parameters)
-    
-def track_order(parameters: dict):
+    intent_handler_dict = {
+        'order.add - context: ongoing-order': add_to_order,
+        'track.order - context: ongoing-tracking': track_order
+    }
+
+    return intent_handler_dict[intent](parameters, session_id)
+
+ 
+def track_order(parameters: dict, session_id: str):
+    """
+    Retrieves the status of the order whether it is 'delivered' or 'in transit'.
+ 
+    Args:
+        parameters (dict): dialogflow parameters.
+        session_id (str): unique id for each chat.
+ 
+    Returns:
+        JSONResponse: it returns the fulfillmentText that has the order status.
+    """
     order_id = int(parameters['number'])
     order_status = db_helper.get_order_status(order_id)
 
@@ -27,6 +46,41 @@ def track_order(parameters: dict):
         fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
     else:
         fulfillment_text = f"No order found with order id : {order_id}"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
+
+
+def add_to_order(parameters: dict, session_id: str):
+    """
+    Getting the food type and the quantity from the customer.
+ 
+    Args:
+        parameters (dict): dialogflow parameters.
+        session_id (str): unique id for each chat.
+ 
+    Returns:
+        JSONResponse: it returns the fulfillmentText with the order details after the customer finishes his order.
+    """
+
+    food_items = parameters["food-item"]
+    quantities = parameters["number"]
+
+    if len(food_items) != len(quantities):
+        fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities clearly?"
+    else:
+        new_food_dict = dict(zip(food_items, quantities))
+
+        if session_id in inprogress_orders:
+            current_food_dict = inprogress_orders[session_id]
+            current_food_dict.update(new_food_dict)
+            inprogress_orders[session_id] = current_food_dict
+        else:
+            inprogress_orders[session_id] = new_food_dict
+
+        order_str = generic_helper.get_str_from_food_dict(inprogress_orders[session_id])
+        fulfillment_text = f"So far you have: {order_str}. Do you need anything else?"
 
     return JSONResponse(content={
         "fulfillmentText": fulfillment_text
